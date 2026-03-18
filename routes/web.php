9e779da81d05ea990943be2d0a7101c400e159a5
VanQuang <vovanuquang01012005@gmail,com>
2026-03-18 17:06:39 +0700

    commit sau khi merge

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,9 @@ Route::get('/vanquang',function(){
 
 Route::get("/giahan","App\Http\Controllers\TenController@ten");
 Route::get("/kimngan","App\Http\Controllers\TenController@ten2");
-=======
+
 Route::get("/tranchau","App\Http\Controllers\TenController@ten1");
 
-=======
 // Bài 7.3: Top 10 doanh thu cao nhất
 Route::get('/top-budget', 'App\Http\Controllers\MovieController@getTopBudget');
 
