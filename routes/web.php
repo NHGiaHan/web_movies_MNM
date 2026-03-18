789002a609c22065dfdd2c62f3c9efcfb622ebab
VanQuang <vovanuquang01012005@gmail,com>
2026-03-18 15:39:49 +0700

    vanquang push

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/',function(){
-    return view('welcome');
+Route::get('/vanquang',function(){
+    return view('VanQuang');
 });
 
 Route::get('/form','App\Http\Controllers\TuoiController@form');
