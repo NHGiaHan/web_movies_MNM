d0cb753b4d65ec007e7afb47c9897da8f5fcbcf8
Kimngan1825 <kimngann1.3.0.6.5@gmail.com>
2026-03-25 21:10:57 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MovieController;
-use App\Http\Controllers\BookController;
 
-use Illuminate\Support\Facades\DB;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,39 +13,12 @@ use Illuminate\Support\Facades\DB;
 |
 */
 
-Route::get('/vanquang', function () {
-    return 'VanQuang';
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get("/giahan", "App\Http\Controllers\TenController@ten");
-Route::get("/kimngan", "App\Http\Controllers\TenController@ten2");
-Route::get("/tranchau", "App\Http\Controllers\TenController@ten1");
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-// 7.1 danh sách các thể loại phim
-Route::get('/the-loai', [MovieController::class, 'listGenres']);
-
-Route::get('/phim-canada', [MovieController::class, 'getCanadaMovies']);
-// 7.2 Top 10 phim có điểm bình chọn cao nhất
-Route::get('/top-10-phim', [MovieController::class, 'topMovies']);
-
-// Bài 7.3: Top 10 doanh thu cao nhất
-Route::get('/top-budget', 'App\Http\Controllers\MovieController@getTopBudget');
-
-// Bài 7.4: Phim dài hơn 120 phút
-Route::get('/long-movies', 'App\Http\Controllers\MovieController@getLongMovies');
-
-// Bài 7.5: Các bộ phim của Canada
-Route::get('/phim-canada', 'App\Http\Controllers\MovieController@getCanadaMovies');
-
-// 7.6 danh sách phim có thể loại là Action
-Route::get('/phim-hanh-dong', 'App\Http\Controllers\MovieController@getActionMovies');
-
-// 7.7 phim có điểm bình chọn trung bình > 8.0 và số lượt bình chọn > 10000
-Route::get('/phim-pho-bien', 'App\Http\Controllers\MovieController@getPopularMovies');
-
-Route::get('sach/chitiet/{id}','App\Http\Controllers\BookController@chitiet');
-// Route hiển thị mặc định (Trang chủ sách)
-Route::get('/sach', 'App\Http\Controllers\LayoutController@sach');
-
-// Route hiển thị theo thể loại
-Route::get('/sach/theloai/{id}', 'App\Http\Controllers\LayoutController@theloai');
+require __DIR__.'/auth.php';
