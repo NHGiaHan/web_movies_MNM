6a622e1741875e95a106a5cec279872b714456f1
Chauchau82 <xiaozhuzhu0205@gmail.com>
2026-03-25 15:49:01 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 Route::get('/vanquang',function(){
     return view('VanQuang');
 });
@@ -19,3 +11,4 @@ Route::get('/vanquang',function(){
 
 Route::get("/giahan","App\Http\Controllers\TenController@ten");
 
+Route::get('/phim-canada', [MovieController::class, 'getCanadaMovies']);
