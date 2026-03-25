b191b66fcc73480f15b71b3383d433c4787aecec
NHGiaHan <ngohaigiahan@gmail.com>
2026-03-25 16:03:25 +0700

    tạo component layout

--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,4 @@ Route::get('/phim-hanh-dong', 'App\Http\Controllers\MovieController@getActionMov
 
 // 7.7 phim có điểm bình chọn trung bình > 8.0 và số lượt bình chọn > 10000
 Route::get('/phim-pho-bien', 'App\Http\Controllers\MovieController@getPopularMovies');
+
